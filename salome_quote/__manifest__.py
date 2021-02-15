e8c0cc0024bf8eaa3ef5a2dad95239b83117e746
Abraham Febres <afebres@xetechs.com>
2021-02-15 12:19:47 -0400

    add: salome qoute

---
+++ salome_quote/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Salome Quote',
+    'version': '1.0',
+    'author': 'Xetechs GT',
+    'website': 'https://xetechs.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale_management'
+    ],
+    'data': [
+        'views/salome_quote_template.xml',
+        'views/ir_action_report.xml',
+    ],
+}
